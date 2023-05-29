8692bf41a696506d7952156815033b6347c6aa0a
LuisCorreaDv <dev.luis.correa@gmail.com>
2023-05-28 22:19:30 -0600

    Creando funcion 'desencriptar' y corrigiendo llaves de encriptacion

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//Leyendo texto con boton "Encriptar"
-
 function encriptar() {
     let textoPrincipal = document.getElementById("texto-principal").value.toLowerCase();
     //bangera i - afecta a mayusculas y minusculas
@@ -7,13 +5,28 @@ function encriptar() {
     //Bandera m - afecta múltiples lineas, palabras, parrafos,etc.
     //Doble barra hace que cada letra se cheque por separado
 
-    var textoEncriptado = textoPrincipal.replace(/e/igm,"imes");
-    var textoEncriptado = textoEncriptado.replace(/a/igm,"emter");
-    var textoEncriptado = textoEncriptado.replace(/i/igm,"ai");
+    var textoEncriptado = textoPrincipal.replace(/e/igm,"enter");
+    var textoEncriptado = textoEncriptado.replace(/a/igm,"ai");
+    var textoEncriptado = textoEncriptado.replace(/i/igm,"imes");
     var textoEncriptado = textoEncriptado.replace(/o/igm,"ober");
     var textoEncriptado = textoEncriptado.replace(/u/igm,"ufat");
 
     document.getElementById("texto-final").value = textoEncriptado;
 }
 
+function desencriptar() {
+    let textoPrincipal = document.getElementById("texto-principal").value.toLowerCase();
+    //bangera i - afecta a mayusculas y minusculas
+    //Bandera g - busca en toda la línea, palabra, oración, etc.
+    //Bandera m - afecta múltiples lineas, palabras, parrafos,etc.
+    //Doble barra hace que cada letra se cheque por separado
+
+    var textoEncriptado = textoPrincipal.replace(/enter/igm,"e");
+    var textoEncriptado = textoEncriptado.replace(/ai/igm,"a");
+    var textoEncriptado = textoEncriptado.replace(/imes/igm,"i");
+    var textoEncriptado = textoEncriptado.replace(/ober/igm,"o");
+    var textoEncriptado = textoEncriptado.replace(/ufat/igm,"u");
+
+    document.getElementById("texto-final").value = textoEncriptado;
+}
 
