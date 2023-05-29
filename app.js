583bfcf578657e399aeb339c74cb788e96328ed0
LuisCorreaDv <dev.luis.correa@gmail.com>
2023-05-28 21:56:03 -0600

    Creando funcion 'encriptar'

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 //Leyendo texto con boton "Encriptar"
 
-function obtenerTexto(){
-    let textoPrincipal = document.getElementById("texto-principal").value;
-    return textoPrincipal;
+function encriptar() {
+    let textoPrincipal = document.getElementById("texto-principal").value.toLowerCase();
+    //bangera i - afecta a mayusculas y minusculas
+    //Bandera g - busca en toda la línea, palabra, oración, etc.
+    //Bandera m - afecta múltiples lineas, palabras, parrafos,etc.
+    //Doble barra hace que cada letra se cheque por separado
+
+    var textoEncriptado = textoPrincipal.replace(/e/igm,"imes");
+    var textoEncriptado = textoEncriptado.replace(/a/igm,"emter");
+    var textoEncriptado = textoEncriptado.replace(/i/igm,"ai");
+    var textoEncriptado = textoEncriptado.replace(/o/igm,"ober");
+    var textoEncriptado = textoEncriptado.replace(/u/igm,"ufat");
+
+    document.getElementById("texto-final").value = textoEncriptado;
 }
+
 
